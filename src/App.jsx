9bd521fac3c08f58f1ffd2e8c48fcb87b56561e4
raulgp <raul.garrigos@hotmail.com>
2023-11-20 17:51:35 +0100

    Iteration 4 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import BoxColor from "./components/BoxColor";
 import Greetings from "./components/Greetings";
 import IdCard from "./components/IdCard";
 import Random from "./components/Random";
@@ -40,6 +41,12 @@ function App() {
 
       <Random min={1} max={6} />
       <Random min={1} max={100} />
+
+      {/* // Iteration 4 */}
+      <h3>BoxColor</h3>
+
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
     </div>
   );
 }
